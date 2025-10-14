62f82d7cf45b5ca9b4d6d29698beca14496fa137
Franco M <damianmaciel0@gmail.com>
2025-10-14 16:52:47 -0300

    chore: setup monorepo structure with husky pre-commit hooks and workspace config
    Se configuró un monorepo multi paquete con husky

--- modules/frontend/lint-staged.config.ts
+++ modules/frontend/lint-staged.config.ts
@@ -3,7 +3,7 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  '*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}': [
+  '*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc,d.ts}': [
     'eslint --cache .',
     'eslint --cache . --fix',
     'prettier --write .'
